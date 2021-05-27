4cc65211f8ca9797bf38920bb62100cec7e966b6
orIsc <orjslash27@gmail.com>
2021-05-27 20:12:58 +0300

    add Login and Register fragments

--- app/src/main/java/com/example/gamerchatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/gamerchatapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.gamerchatapp;
+package com.example.gamerchatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.gamerchatapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
